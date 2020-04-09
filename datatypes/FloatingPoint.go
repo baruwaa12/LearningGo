4523432ee36364155eb57db15ea8d3d710fdda20
Ademola <baruwaademola@outlook.com>
2020-04-09 17:19:57 +0100

    Drawing of the Mandelbrot set

--- datatypes/FloatingPoint.go
+++
@@ -1,8 +0,0 @@
-package FloatingPoint
-
-for x := 0; x < 8; x++ {
-	fmt.Printf("x = %d eA = %8.3f\n", x, math.Exp(float64(x)))
-	}
-
-var z float64
-fmt.Println(z, -1, 1/z, -1/z, z/z) "0 -0 +Inf -Inf NaN"
