3ac5dac21d23b0220e1a4ba3facfc7a9220f3b31
Ademola <baruwaademola@outlook.com>
2020-05-18 19:43:23 +0100

    Convert all letters to lowercase

--- Exercises/crypto1/cryptosquare.go
+++ Exercises/crypto1/cryptosquare.go
@@ -2,6 +2,7 @@ package cryptosquare
 
 import (
 	"fmt"
+	"strings"
 )
 
 
@@ -10,12 +11,15 @@ import (
 // Loop through string to check for any empty spaces
 // Append letters to empty variable called newstr
 // Remove any other possible punctuation
+// Convert all letters to lowercase
 
 func remover(str1 string) (newstr string) {
 	for i := 0; i < len(str1); i++ {
 		if str1[i] != ' ' && str1[i] != ','  && str1[i] != ';' && str1[i] != ':'{
 			newstr = newstr + string(str1[i])
-			fmt.Println(newstr)
+			// fmt.Println(newstr)
+			finalresult := strings.ToLower(newstr)
+			fmt.Println(finalresult)
 		}
 	}
 	return newstr
