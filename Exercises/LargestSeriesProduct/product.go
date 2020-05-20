721f493e30338d11f5c9554c2af803d7591a8cf8
Ademola <baruwaademola@outlook.com>
2020-05-20 15:06:48 +0100

    Largest series product exercise completed

--- Exercises/LargestSeriesProduct/product.go
+++ Exercises/LargestSeriesProduct/product.go
@@ -13,23 +13,43 @@ func collectnumber() {
 	// number, _ := reader.ReadString('\n')
 }
 
-func ndecider(mynumber string) {
+// Calculate the length of the number
+// Set the values of current and largest so they can be reset outside of each iteration
+// of any span length
+// Loop through the number string given the span length
+// Calculate the largest and current value then compare and store the largest 
+// Continue until loop is completed.
+
+func ndecider(mynumber string, span int) (int) {
 	fmt.Println(len(mynumber))
 
-	for i := 0; i <= len(mynumber)-3; i++ {
-		a, _ := strconv.Atoi(string(mynumber[i]))
-		b, _ := strconv.Atoi(string(mynumber[i + 1]))
-		c, _ := strconv.Atoi(string(mynumber[i + 2]))
-		fmt.Println(a)
-		fmt.Println(b)
-		fmt.Println(c)
+	current := 1
+	largest := 1
+
+
+	for i := 0; i < len(mynumber)-span; i++ {
+		current = 1
+		for z := 0; z < span; z++ {
+			a, _ := strconv.Atoi(string(mynumber[i+z]))
+			if i == 0 {
+				largest = largest * a
+			} 
+			current = current * a
+		}
+
+		if largest < current{
+			largest = current
+	
+		}
+
 		fmt.Println("-----------")
 
 	}
-
+	return largest
 }
 
 func main() {
 	fmt.Println("Done")
-	ndecider("1023344898123")
+	result := ndecider("102330", 2)
+	fmt.Println(result)
 }
