8cb05e139e94cbef2784ffcc140b5d288bd4ff44
Ademola <baruwaademola@outlook.com>
2020-05-22 12:04:18 +0100

    Acroynm V2 complete

--- Exercises/acroynm/ac/acronym1.go
+++ Exercises/acroynm/ac/acronym1.go
@@ -5,12 +5,20 @@ import (
 	"strings"
 )
 
-func acroynm (input string) {
+
+func acroynm (input string) (string) {
 	words := strings.Fields(input)
 	fmt.Println(words, len(words))
+	var result string
+	for _, word := range words {
+		// fmt.Println(string(word[0]))
+		result = result + string(word[0])
+		// fmt.Println(result)
+	}
+	return result
 }
 
 func main() {
-	acroynm("Ade is eighteen")
+	fmt.Println(acroynm("Ade is eighteen"))
 	fmt.Println("Done")
 }
