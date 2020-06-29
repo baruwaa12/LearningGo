811a87e2378d7bf9ec47a16f40ba6f178cd316b6
Ademola <baruwaademola@outlook.com>
2020-06-29 14:21:30 +0100

    Palindromic exercise completed

---
+++ Exercises/palindromicproducts/palindromic.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Takes a start and end value.
+// Loops through 2 seperate arrays and finds all products within the given range
+func palinchecker(start int, end int) []int {
+	factors := []int{}
+	for i := start; i <= end; i++ {
+		for j := start; j <= end; j++ {
+			factors = append(factors, (i * j))
+
+		}
+	}
+	return factors
+}
+
+// Takes the factors array and removes any duplicate values
+// Then creates a seperate array and appends new values
+func unique(factors []int) []int {
+	keys := make(map[int]bool)
+	list := []int{}
+	for _, entry := range factors {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
+
+func main() {
+
+	fmt.Println(unique(palinchecker(1, 5)))
+}
