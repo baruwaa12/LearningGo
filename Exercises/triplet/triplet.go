1b515304e556a53187f683fbc2fcba43e4bb95b9
Ademola <baruwaademola@outlook.com>
2020-07-21 15:01:44 +0100

    Triplet test completed

--- Exercises/triplet/triplet.go
+++ Exercises/triplet/triplet.go
@@ -1,7 +1,19 @@
 package triplet
 
+
 func tripletchecker(a int, b int, c int) int {
 	var total int
-	total = 25
+
+	// if a < b < c
+	// and if a + b +  c = 1000
+	// and if a**2 + b**2 = c**2
+
+	if (a < b && b < c) && (a*a + b*b == c*c){
+		total = (c*c)
+	}
+
+
 	return total
+
+
 }
--- Exercises/triplet/triplet_test.go
+++ Exercises/triplet/triplet_test.go
@@ -8,7 +8,7 @@ func TestTripletChecker(t *testing.T) {
 	for _, test := range PythagoreanTriplets {
 		var result = tripletchecker(test.a, test.b, test.c);
 		if result != test.total {
-			t.Fatalf("FAILED given a %v b %v c %v", test.a, test.b, test.c)
+			t.Fatalf("FAILED given a %v b %v c %v returned %v expected %v", test.a, test.b, test.c, result, test.total)
 		}
 		t.Logf("Passed %s", test.description)
 	}
